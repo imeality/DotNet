6f67b6f39909b659fe42f57a116c008a74a55b11
shivanip2507 <shivaniporwal@imeality.com>
2019-03-04 19:06:16 +0530

    TokenBasedAuthentication

---
+++ TokenBased/TokenBased/Models/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenBased.Models
+{
+    public class UserRepository:IDisposable
+    {
+        CvaniEntities context = new CvaniEntities();
+        public UserMaster ValidateUser(string username,string password)
+        {
+            return context.UserMasters.FirstOrDefault(user => user.UserName.Equals(username) && user.UserPassword == password);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
